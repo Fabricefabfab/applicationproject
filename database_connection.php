e932d4920731743b66a4908bb5232110d3eb3c77
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2023-05-04 12:03:19 +0200

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -3,7 +3,7 @@
 $server = "us-cdbr-east-06.cleardb.net";
 $user = "bedb62f2f3a0f9";
 $pass = "222e3331";
-$database = "capstone";
+$database = "heroku_dfdc5cf28c06aa6";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
